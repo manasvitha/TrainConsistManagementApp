0f1e0b344bfd6229a9ee5d11a1140c2e773d6a1b
manasvitha <manasvitha653@gmail.com>
2026-04-02 14:34:07 +0530

    uc1

---
+++ App/src/TrainConsistManagementApp.java
@@ -0,0 +1,15 @@
+public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+        System.out.println("===============================================");
+        System.out.println(" == Train Consist Management App == ");
+        System.out.println("===============================================\n");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial Bogie Count : " + trainConsist.size());
+        System.out.println("Current Train Consist : " + trainConsist);
+        System.out.println("\nSystem ready for operations...");
+    }
+
+}
